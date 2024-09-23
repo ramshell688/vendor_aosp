LineageOS/android_vendor_lineage

config: tablet: Enable freeform and large screen support for all targets
Change-Id: I8137911f96cf2eb347d8e5012be649ddb01057f8

config/common_mini_tablet.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_mini.mk)
 
+# Inherit tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/tablet.mk)
+
 $(call inherit-product, vendor/lineage/config/telephony.mk)

config/common_mini_tablet_wifionly.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_mini.mk)
 
+# Inherit tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/tablet.mk)
+
 $(call inherit-product, vendor/lineage/config/wifionly.mk)

ramshell688/vendor_aosp

config/common_mini_tablet.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common Blaze stuff
 $(call inherit-product, vendor/blaze/config/common_mobile_mini.mk)
 
+# Inherit tablet common Blaze stuff
+$(call inherit-product, vendor/blaze/config/tablet.mk)
+
 $(call inherit-product, vendor/blaze/config/telephony.mk)

config/common_mini_tablet_wifionly.mk
@@ -1,2 +1,5 @@
 # Inherit mobile mini common Blaze stuff
 $(call inherit-product, vendor/blaze/config/common_mobile_mini.mk)
+
+# Inherit tablet common Blaze stuff
+$(call inherit-product, vendor/blaze/config/tablet.mk)
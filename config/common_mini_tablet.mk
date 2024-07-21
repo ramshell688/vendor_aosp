47ba9e3d0d64c978fa00da2a9fea82b1820f6aba
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 22:01:35 +0200

    config: tablet: Enable freeform and large screen support for all targets
    Change-Id: I8137911f96cf2eb347d8e5012be649ddb01057f8

--- config/common_mini_tablet.mk
+++ config/common_mini_tablet.mk
@@ -1,4 +1,7 @@
 # Inherit mobile mini common Blaze stuff
 $(call inherit-product, vendor/blaze/config/common_mobile_mini.mk)
 
+# Inherit tablet common Blaze stuff
+$(call inherit-product, vendor/blaze/config/tablet.mk)
+
 $(call inherit-product, vendor/blaze/config/telephony.mk)
--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -1,2 +1,5 @@
 # Inherit mobile mini common Blaze stuff
 $(call inherit-product, vendor/blaze/config/common_mobile_mini.mk)
+
+# Inherit tablet common Blaze stuff
+$(call inherit-product, vendor/blaze/config/tablet.mk)
